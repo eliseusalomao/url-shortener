121e0ea6833927d5248b2f44e02f709db433d51a
eliseusalomao <eliseusalomao20@gmail.com>
2024-06-15 15:51:46 -0300

    fix: fix tr directly under table

--- frontend/src/components/table.tsx
+++ frontend/src/components/table.tsx
@@ -22,10 +22,12 @@ export function Table () {
 
     return (
         <table className="w-full text-sm border-t-2 border-b-2 border-slate-950">
-            <tr className="border-b border-teal-950">
-                <th className="text-left py-3 px-4 font-medium">Link encurtado</th>
-                <th className="text-left py-3 px-4 font-medium">Link original</th>
-            </tr>
+            <thead>
+                <tr className="border-b border-teal-950">
+                    <th className="text-left py-3 px-4 font-medium">Link encurtado</th>
+                    <th className="text-left py-3 px-4 font-medium">Link original</th>
+                </tr>
+            </thead>
             <tbody className="[&_tr:last-child]:border-0 [&_tr:hover]:bg-zinc-800/50">
                 {linksResponse?.map((tag) => {
                     return (

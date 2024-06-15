8f9cdfa602b398c2ac41adf8c73f383247c1d5d3
eliseusalomao <eliseusalomao20@gmail.com>
2024-06-15 15:27:34 -0300

    feat: add request to list all links

--- frontend/src/components/table.tsx
+++ frontend/src/components/table.tsx
@@ -1,4 +1,25 @@
+import { useQuery } from "@tanstack/react-query"
+
+interface Tag {
+    id: string
+    code: string
+    original_url: string
+}
+
 export function Table () {
+    const { data: linksResponse, isLoading } = useQuery<Tag[]>({
+        queryKey: ['get-links'],
+        queryFn: async () => {
+            const response = await fetch('http://localhost:3333/api/links')
+            const data = await response.json()
+            return data
+        },
+    })
+
+    if (isLoading) {
+        return null
+    }
+
     return (
         <table className="w-full text-sm border-t-2 border-b-2 border-slate-950">
             <tr className="border-b border-teal-950">
@@ -6,11 +27,11 @@ export function Table () {
                 <th className="text-left py-3 px-4 font-medium">Link original</th>
             </tr>
             <tbody className="[&_tr:last-child]:border-0 [&_tr:hover]:bg-zinc-800/50">
-                {Array.from({ length: 10 }).map((_, idx) => {
+                {linksResponse?.map((tag) => {
                     return (
-                        <tr key={idx}>
-                            <td className="font-medium py-3 px-4">Wikipedia</td>
-                            <td className="font-medium py-3 px-4"><span className="text-xs text-zinc-400">https://www.wikipedia.org/</span></td>
+                        <tr key={tag.id}>
+                            <td className="font-m edium py-3 px-4">{tag.code}</td>
+                            <td className="font-medium py-3 px-4"><span className="text-xs text-zinc-400">{tag.original_url}</span></td>
                         </tr>
                     )
                 })}

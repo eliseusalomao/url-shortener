01653d31b0a652356f046b330e47463f9aef5f0f
eliseusalomao <eliseusalomao20@gmail.com>
2026-07-28 12:31:48 -0300

    chore: update dependencies and fix security vulnerabilities

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,22 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { App } from './App.tsx'
-import './index.css'
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import { App } from "./App.tsx";
+import "./index.css";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />
-    }
-])
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <RouterProvider router={router} />
-        </QueryClientProvider>
-    </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </React.StrictMode>,
+);
